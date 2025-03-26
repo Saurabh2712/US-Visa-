7ca18c58c53aa19a3764c75e6a4b2a73a403337d
saurabh <96339810+Saurabh2712@users.noreply.github.com>
2025-03-26 15:01:29 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="us_visa",
     version="0.0.0",
-    author="Bappy",
-    author_email="entbappy73@gmail.com",
+    author="Saurabh",
+    author_email="saurabhgahukar47@gmail.com",
     packages=find_packages()
 )
